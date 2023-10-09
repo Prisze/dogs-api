25c72a91e305a8997e85e99f7ba1085380404f78
Priscilla <priscilla.marquez@outlook.com>
2023-10-08 23:32:32 -0500

    morechanges

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dogs-api/',
+  base: '/vue-restapi-demo/',
   plugins: [vue()],
   resolve: {
     alias: {
